2ca8817e43e450ff58017a8e6963b4a1c2d14ecf
egomez <eduardgb92@gmail.com>
2023-09-23 10:49:30 +0200

    File structure set

---
+++ src/handler.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+
+function component(){
+    const element = document.createElement("div");
+
+    element.innerHTML = "Hello world";
+
+    return element;
+}
+
+document.body.appendChild(component());
