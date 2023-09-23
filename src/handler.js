86d44ac603ebe47573a9cf0665937a8b02549243
egomez <eduardgb92@gmail.com>
2023-09-23 12:54:10 +0200

    Basic homepage

--- src/handler.js
+++ src/handler.js
@@ -1,12 +1,46 @@
 import './style.css'
+import arrow from './assets/icons/proximo.png';
 
 
-function component(){
-    const element = document.createElement("div");
 
-    element.innerHTML = "Hello world";
 
-    return element;
-}
 
-document.body.appendChild(component());
+const container = document.createElement("div");
+container.setAttribute("id", "container");
+
+const blackBar = document.createElement("div");
+blackBar.setAttribute("id", "black-bar");
+
+const lateralBars = document.createElement("div");
+lateralBars.setAttribute("id", "lateral-bars");
+
+const mainName = document.createElement("div");
+mainName.setAttribute("id", "main-name");
+mainName.innerHTML = "EDI GOMEZ";
+
+const arrowRight = document.createElement("img");
+arrowRight.setAttribute("id", "arrow");
+arrowRight.src = arrow;
+
+
+const projectBar = document.createElement("div");
+projectBar.setAttribute("id", "project");
+
+const aboutMeBar = document.createElement("div");
+aboutMeBar.setAttribute("id", "about-me");
+
+const contactBar = document.createElement("div");
+contactBar.setAttribute("id", "contact");
+
+
+
+blackBar.appendChild(arrowRight);
+container.appendChild(mainName);
+lateralBars.appendChild(projectBar);
+lateralBars.appendChild(aboutMeBar);
+lateralBars.appendChild(contactBar);
+container.appendChild(lateralBars)
+container.appendChild(blackBar);
+document.body.appendChild(container);
+
+
